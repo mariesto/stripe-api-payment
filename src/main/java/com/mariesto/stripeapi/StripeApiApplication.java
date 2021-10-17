3182e2e1e49006d3339ad33148610b4164f462d3
Amendo Mariesto Sitinjak <sitinjakamendo@gmail.com>
2021-10-17 16:51:53 +0700

    [init] Initial commit

---
+++ src/main/java/com/mariesto/stripeapi/StripeApiApplication.java
@@ -0,0 +1,12 @@
+package com.mariesto.stripeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripeApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StripeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mariesto/stripeapi/StripeApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.mariesto.stripeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripeApiApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
